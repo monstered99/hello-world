3bf2cde4fe74f8ae3bab37e003bac3354e61e7ab
monstered99 <monstered99@gmail.com>
2018-07-10 14:05:59 +0200

    Added some more examples

--- new.go
+++ new.go
@@ -1,9 +1,52 @@
 // You can edit this code!
-// Click here and start typing.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+/*
+func add(x, y float64) float64 {
+	return x + y
+}
+
+func multiple(a, b string) (string, string) {
+	return a, b
+}
+*/
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Whoa, Go is neat!")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Expert web design by Sentdex")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	//var num1, num2 float64 = 5.6, 9.5
+	//fmt.Println(add(num1, num2))
+	/*w1, w2 := "Hey", "there"
+	fmt.Println(multiple(w1, w2))
+	x := 15
+	a := &x
+	fmt.Println(x, a, *a)
+	*/
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/about/", aboutHandler)
+	http.ListenAndServe(":8000", nil)
 }
+
+//Pierwiastek drugiego stopnia z n
+/*
+func main() {
+	fmt.Println("Square root of 4 = ", math.Sqrt(4))
+}
+*/
+
+//RNG od 1 do 100
+/*
+func main() {
+	fmt.Println("A number from 1-100", rand.Intn(100))
+}
+*/
